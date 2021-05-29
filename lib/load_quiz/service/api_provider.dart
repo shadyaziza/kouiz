141cc2cde020be42cb95f3337951ff8b3bb653e9
shadyaziza <shadyaziza@gmail.com>
2021-05-29 17:22:20 +0200

    fix dio crashing issue

--- lib/load_quiz/service/api_provider.dart
+++ lib/load_quiz/service/api_provider.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
-import 'dart:convert';
+// import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:kouiz/load_quiz/model/quiz.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 final apiProvider =
     Provider.family<Api, QuizSettings>((ref, settings) => Api(settings));
@@ -15,18 +16,13 @@ final quizFutureProvider =
   return response;
 });
 
-class GetQuizExcpetion {
-  @override
-  String toString() {
-    return 'Error in fetching quiz';
-  }
-}
-
 class Api {
   final QuizSettings settings;
   Api(this.settings);
   final apiKey = 'irmjzteXpf36YHysVLxWZgAhaZC1rD8T2KSP9xb5';
-
+  final _url = 'https://quizapi.io/api/v1/questions/';
+  final CancelToken cancelToken = CancelToken();
+  final dio = Dio();
   Future<Quiz> getQuiz() async {
     try {
       return await _getQuiz(false);
@@ -35,23 +31,65 @@ class Api {
     }
   }
 
-  final client = http.Client();
-
   Future<Quiz> _getQuiz(bool retry) async {
-    var _url = Uri.parse('https://quizapi.io/api/v1/questions/');
-    _url = _url.replace(queryParameters: settings.toMap(retry: retry));
+    final response = await dio.get(_url,
+        options: Options(
+          // followRedirects: false,
+          validateStatus: (statusCode) => statusCode! < 500,
+          headers: {
+            'X-Api-Key': apiKey,
+            HttpHeaders.contentTypeHeader: 'application/json'
+          },
+        ),
+        queryParameters: settings.toMap(retry: retry));
+    final _res = _response(response);
 
-    final response = await http.get(
-      _url,
-      headers: {
-        'X-Api-Key': apiKey,
-        HttpHeaders.contentTypeHeader: 'application/json'
-      },
-    ).catchError((_) async {
-      await _getQuiz(true);
-    });
+    Quiz _quiz = Quiz.fromMap(response.data);
 
-    Quiz _quiz = Quiz.fromMap(jsonDecode(response.body));
     return _quiz;
   }
+
+  _response(Response response) {
+    switch (response.statusCode) {
+      case 200:
+        return response;
+      case 401:
+        throw UnauthorisedException(response.data.toString());
+      case 404:
+        throw InvalidInputException(response.data.toString());
+      case 429:
+        throw BadRequestException(response.data.toString());
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode: ${response.statusCode}');
+    }
+  }
+}
+
+class CustomException implements Exception {
+  final _message;
+  final _prefix;
+
+  CustomException([this._message, this._prefix]);
+
+  String toString() {
+    return "$_prefix$_message";
+  }
+}
+
+class FetchDataException extends CustomException {
+  FetchDataException([String? message])
+      : super(message, "Error During Communication: ");
+}
+
+class BadRequestException extends CustomException {
+  BadRequestException([message]) : super(message, "Invalid Request: ");
+}
+
+class UnauthorisedException extends CustomException {
+  UnauthorisedException([message]) : super(message, "Unauthorised: ");
+}
+
+class InvalidInputException extends CustomException {
+  InvalidInputException([String? message]) : super(message, "Invalid Input: ");
 }
